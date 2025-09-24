92ac21cce5bd5bc04be2155986c713d8170da7dd
matiaspietrafesa1 <mp318802@fi365.ort.edu.uy>
2025-09-24 17:24:20 -0300

    crearTransaccion completo

--- api/v1/middlewares/auth.middleware.js
+++ api/v1/middlewares/auth.middleware.js
@@ -14,9 +14,9 @@ export const authenticateMiddleware = (req, res, next) => {
         if(err){
             return res.status(403).json({ error:"Token no válido"})
         }else{
-            req.body.username = user.username;
+            //req.body.username = user.username;
             req.body.userId = user.id;
-            req.body.plan = user.plan;
+            //req.body.plan = user.plan;
             next();
         }
     })
