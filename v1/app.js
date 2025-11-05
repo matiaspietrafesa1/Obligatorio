a0999d550b52286188d1bcfe57fe5635c86814ed
matiaspietrafesa1 <mp318802@fi365.ort.edu.uy>
2025-11-05 08:30:08 -0300

    Fix: update CORS configuration to allow requests from additional origins

--- v1/app.js
+++ v1/app.js
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://obligatorio-front-end.vercel.app"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
